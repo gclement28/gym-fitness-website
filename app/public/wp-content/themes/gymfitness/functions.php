0486eac9befa06100dae5c47edfd333cc9df536b
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2022-09-12 18:11:20 +0200

    Second template and page loop done, code is refactored

--- app/public/wp-content/themes/gymfitness/functions.php
+++ app/public/wp-content/themes/gymfitness/functions.php
@@ -61,3 +61,22 @@ function gymfitness_setup()
 }
 
 add_action('after_setup_theme', 'gymfitness_setup');
+
+
+// create a widget zone
+
+function gymfitness_widgets()
+{
+  register_sidebar(
+    array(
+      'name' => 'Sidebar',
+      'id' => 'sidebar',
+      'before_widget' => '<div class="widget">',
+      'after_widget' => '</div>',
+      'before_title' => '<h3>',
+      'after_title' => '</h3>'
+    )
+  );
+}
+
+add_action('widgets_init', 'gymfitness_widgets');
--- app/public/wp-content/themes/gymfitness/page.php
+++ app/public/wp-content/themes/gymfitness/page.php
@@ -2,20 +2,7 @@
 
 <main class="container page section no-sidebars">
 
-  <?php while (have_posts()) : the_post(); ?>
-    <h1 class="text-center text-primary"><?php the_title(); ?></h1>
-    <?php
-    if (has_post_thumbnail()) :
-      the_post_thumbnail('blog', array('class' => 'featured-image'));
-
-    endif;
-    ?>
-    <div class="text-center">
-      <?php the_content(); ?>
-    </div>
-
-
-  <?php endwhile ?>
+  <?php get_template_part('template-parts/page', 'loop'); ?>
 
 </main>
 
