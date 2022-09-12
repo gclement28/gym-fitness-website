7a3bb7d558af780c44edd4ec1bccec43ad864006
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2022-09-12 17:36:03 +0200

    Page template is done

--- app/public/wp-content/themes/gymfitness/functions.php
+++ app/public/wp-content/themes/gymfitness/functions.php
@@ -42,3 +42,22 @@ wp_enqueue_script('slicknavjs', get_template_directory_uri() . '/js/jquery.slick
 wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true);
 
 add_action('wp_enqueue_scripts', 'gymfitness_scripts');
+
+//Enable feature images and other
+
+function gymfitness_setup()
+{
+
+  // register new image size 
+
+  add_image_size('square', 350, 350, true);
+  add_image_size('portrait', 350, 724, true);
+  add_image_size('box', 400, 375, true);
+  add_image_size('mediumSize', 700, 400, true);
+  add_image_size('blog', 966, 644, true);
+
+  //add feature image
+  add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'gymfitness_setup');
--- app/public/wp-content/themes/gymfitness/page.php
+++ app/public/wp-content/themes/gymfitness/page.php
@@ -1,9 +1,22 @@
 <?php get_header(); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <h1><?php the_title(); ?></h1>
-  <?php the_content(); ?>
+<main class="container page section no-sidebars">
 
-<?php endwhile ?>
+  <?php while (have_posts()) : the_post(); ?>
+    <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+    <?php
+    if (has_post_thumbnail()) :
+      the_post_thumbnail('blog', array('class' => 'featured-image'));
+
+    endif;
+    ?>
+    <div class="text-center">
+      <?php the_content(); ?>
+    </div>
+
+
+  <?php endwhile ?>
+
+</main>
 
 <?php get_footer(); ?>
