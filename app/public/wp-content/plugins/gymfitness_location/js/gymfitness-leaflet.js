c0376632bba5017ac92362b9ab1f4785efaf8333
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2022-09-14 18:35:19 +0200

    the contact us page is done, I added a map from leaflet using acf

---
+++ app/public/wp-content/plugins/gymfitness_location/js/gymfitness-leaflet.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const lat = document.querySelector('#lat').value;
+  const lng = document.querySelector('#lng').value;
+  const address = document.querySelector('#address').value;
+
+  if (lat && lng) {
+    var map = L.map('map').setView([lat, lng], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map).bindPopup(address).openPopup();
+  }
+});
