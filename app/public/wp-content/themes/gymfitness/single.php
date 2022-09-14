bd66b0d0521ee275f9f788b731587fc57a50a78f
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2022-09-14 16:28:00 +0200

    Blog is done

--- app/public/wp-content/themes/gymfitness/single.php
+++ app/public/wp-content/themes/gymfitness/single.php
@@ -1,5 +1,11 @@
-<?php while (have_posts()) : the_post(); ?>
-  <h1><?php the_title(); ?></h1>
-  <?php the_content(); ?>
+<?php get_header(); ?>
+<main class="container page section with-sidebar">
 
-<?php endwhile ?>
+  <div class="page-content">
+    <?php get_template_part('template-parts/page', 'loop'); ?>
+  </div>
+
+  <?php get_sidebar(); ?>
+
+</main>
+<?php get_footer(); ?>
